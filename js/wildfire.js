c0ed9c392f70cd18a15d2ca7f4684d3ad34a322a
Dilshan Fernando <dilshanfernando161@gmail.com>
2025-09-24 23:49:40 +1000

    .

--- js/wildfire.js
+++ js/wildfire.js
@@ -1,3 +1,5 @@
-var world_map = 'world_map_fire.vg.json'
+ var world_map = "js/world_map_fire.vg.json";
 
-vegaEmbed("#world_map",world_map).then(function(result{}).catch(console.error);
+ vegaEmbed('#world_map', world_map).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+  }).catch(console.error);
