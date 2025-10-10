6ef22c3955c4fd3b8ca6950e2fd12486aa8948db
Dilshan Fernando <dilshanfernando161@gmail.com>
2025-10-10 12:51:31 +1100

    HW week 10 finished

--- js/wildfire.js
+++ js/wildfire.js
@@ -1,10 +1,15 @@
  var world_map = "js/world_map_fire.vg.json";
  var interactive_map = "js/interactive_visualization.vg.json";
+ var week10_map = "js/week10.vg.json";
 
  vegaEmbed('#world_map', world_map).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
   }).catch(console.error);
 
  vegaEmbed('#interactive_map', interactive_map).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+  }).catch(console.error);
+
+  vegaEmbed('#week10', week10_map).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
   }).catch(console.error);
