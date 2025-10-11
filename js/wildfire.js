aa323bf7bd75f33f6127f259198348f2420f73bc
Dilshan Fernando <dilshanfernando161@gmail.com>
2025-10-11 14:40:13 +1100

    changing some of the data files to match names

--- js/wildfire.js
+++ js/wildfire.js
@@ -1,6 +1,7 @@
  var world_map = "js/world_map_fire.vg.json";
  var interactive_map = "js/interactive_visualization.vg.json";
  var week10_map = "js/week10.vg.json";
+ var global_temp = "js/global_trend_temperature.vg.json";
 
  vegaEmbed('#world_map', world_map).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
@@ -11,5 +12,9 @@
   }).catch(console.error);
 
   vegaEmbed('#week10', week10_map).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+  }).catch(console.error);
+
+  vegaEmbed('#global_temp', global_temp).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
   }).catch(console.error);
