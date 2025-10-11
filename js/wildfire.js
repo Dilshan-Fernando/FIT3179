a0ef9bf3a3e9fdc3c80dfe460be3597dbe6a7acd
Dilshan Fernando <dilshanfernando161@gmail.com>
2025-10-11 15:11:53 +1100

    adding more wildfire data

--- js/wildfire.js
+++ js/wildfire.js
@@ -2,6 +2,7 @@
  var interactive_map = "js/interactive_visualization.vg.json";
  var week10_map = "js/week10.vg.json";
  var global_temp = "js/global_trend_temperature.vg.json";
+ var global_rain = "js/global_trend_rain.vg.json";
 
  vegaEmbed('#world_map', world_map).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
@@ -16,5 +17,9 @@
   }).catch(console.error);
 
   vegaEmbed('#global_temp', global_temp).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+  }).catch(console.error);
+
+  vegaEmbed('#global_rain', global_rain).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
   }).catch(console.error);
